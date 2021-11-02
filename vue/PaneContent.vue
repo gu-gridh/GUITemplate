bba1ac435c65ac4bd50439d48fd6822da7f9b998
Arild Matsson <arild.matsson@gu.se>
2021-11-02 17:08:14 +0100

    Pane

---
+++ vue/PaneContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pane-content">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaneContent",
+};
+</script>
+
+<style>
+.pane-content {
+  padding: 15px 30px;
+}
+
+.pane-content > :first-child {
+  margin-top: 0;
+}
+
+.pane-content > :last-child {
+  margin-bottom: 0;
+}
+</style>
